a5b164ce950afadffc1049c27cb4307ac14dcacc
Olabode Michael <56226335+mike-kayode@users.noreply.github.com>
2024-08-23 07:22:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const ownervb = process.env.OWNERS || "917605902011;GURU;6281255369012;Ash"
+const ownervb = process.env.OWNERS || "2348108727382;Mike;2348108727382;Mike"
 
 
 const ownerlist = ownervb.split(';');
